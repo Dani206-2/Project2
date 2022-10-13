6742e5cf0a2812702e4195f58afa7f72847db5f0
Dani206-2 <114012629+Dani206-2@users.noreply.github.com>
2022-10-13 16:18:06 +0100

    Latest

--- Practical 2 draft.R
+++ Practical 2 draft.R
@@ -5,7 +5,7 @@
 ### sequence of the prisoner going through the boxes with the corresponding key
 ### card. 
 
-### The input is n: the number of prisoners, k: the first key number 
+### The input is n: the number of prisoners, k: the prison number 
 ### strategy: 3 strategies to choose, nreps.nreps: number of simulations, the 
 ### default value is set to 10,000
 
@@ -14,28 +14,25 @@
 Pone<-function (n, k, strategy, nreps.nreps=10000){
   ### We set the number of successes as 0
   success<-0
+  prisseq<-seq(1,2*n) ### an ascending sequence of all prisoners' number
   
   ### Repeating the simulation for nreps.nreps times
   for (i in 1:nreps.nreps){
-    
-    prisnum<-sample(2*n,1)  ### Randomly assign a number for the prisoner
     cardseq<-rep(0,times=2*n) ### Generate an empty vector for the card numbers
-    prisseq<-seq(1,2*n) ### an ascending sequence of all prisoners' number
+    cardseq[] <- sample(prisseq,2*n) ### Randomly allocate card no. to boxes
+    
    
      ### Enters if the strategy is 1 or 2
     if(strategy==1|strategy==2){
      
       
+      
       if(strategy==1){ ### for strategy 1
-        prisseq<-prisseq[-k] ### We remove the value k in the prisoner seq 
-        cardseq[prisnum]<-k ### Assign k to the box with the prisoner's num
         
-        ### Randomly allocate other card number to other boxes
-        cardseq[-prisnum]<-sample(prisseq,2*n-1)  
-        opencard<-k ### The first card to be opened
+        opencard<-cardseq[k] ### The first card to be opened
         
       } else { ### for strategy 2
-        cardseq[] <- sample(prisseq,2*n) ### Randomly allocate card no. to boxes
+        
         boxopen<-sample(prisseq,1) ### The box that the prisoner opens 
         opencard<-cardseq[boxopen] ### The card number in that box
       }
@@ -44,7 +41,7 @@ Pone<-function (n, k, strategy, nreps.nreps=10000){
       i<-1 ### Set the number of boxes open to be 1
       
       ### When the card is still not found and boxes opened is less than n
-      while (opencard!=prisnum & i<n){
+      while (opencard!=k & i<n){
         ### Update the card opened by using the previous card number to find
         ### the corresponding box number
         opencard<-cardseq[opencard] 
@@ -52,27 +49,53 @@ Pone<-function (n, k, strategy, nreps.nreps=10000){
       }
       
       ### If the prisoner finds the card, then succeed!
-      if(opencard==prisnum){
+      if(opencard==k){
         success<-success+1
       }
     }
     
     if (strategy==3){
-      cardseq[] <- sample(prisseq,2*n) ## Randomly allocate card no. to boxes
       openseq<-sample(prisseq,n) ### Generate a sequence of boxes to be opened
       
-      ### if the card no is in one of  the n boxes, it counts as success
-      if (prisnum %in% cardseq[openseq]){
+      ### if the prison number is in one of the n boxes, it counts as success
+      if (k %in% cardseq[openseq]){
         success<-success+1
         
       }
     }
     
-    
-    
   }
-  
   
  success/nreps.nreps ### Output:the probability of success
   
 }
+
+
+
+Pall<-function(...){
+  k<- sample(1,2)
+  
+  ### what to change in Pone
+  ### 
+  ### strategy 1: no more k
+  ### cardseq[]<-sample(prisseq,2*n-1)  
+  ### opencard<-cardseq[prisnum]      
+  
+  ### Apply function????
+  
+  
+}
+
+
+
+dloop<-function(n,nreps){
+  
+  
+  
+  
+  
+}
+
+
+
+
